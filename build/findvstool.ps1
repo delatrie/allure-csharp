b6dfdc4a1e071fe48468a4f1556fb65e96470550
Maksim Stepanov <17935127+delatrie@users.noreply.github.com>
2025-10-03 04:01:32 +0700

    fix locked file and scripts

--- build/findvstool.ps1
+++ build/findvstool.ps1
@@ -5,32 +5,45 @@ Param
     [string] $ToolName
 )
 
+$DEFAULT_VSWHERE_PATH = "${Env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe"
+
 If (-not $IsWindows)
 {
-    Write-Error "Can't run this script on $($PSVersionTable.Platform). Windows is required"
-    Return
+    Throw "Can't run this script on $($PSVersionTable.Platform). Windows is required"
 }
 
-$VsInstallPath = vswhere.exe -latest -property installationPath
+$VsWhere = ("vswhere.exe", $DEFAULT_VSWHERE_PATH) | ForEach-Object {
+    Get-Command -CommandType Application -ErrorAction SilentlyContinue $_
+} | Select-Object -first 1
+
+If (-not $VsWhere)
+{
+    Throw "vswhere.exe needs to be in PATH or at '$DEFAULT_VSWHERE_PATH'"
+}
+
+$VsInstallPath = & $VsWhere -latest -property installationPath
+If (-not $VsInstallPath)
+{
+    Throw "Can't resolve a Visual Studio installation path. vswhere.exe exited with code $LASTEXITCODE"
+}
+
 $LaunchDevToolPath = Join-Path $VsInstallPath 'Common7\Tools\Launch-VsDevShell.ps1'
 
 if (-not (Test-Path -PathType Leaf $LaunchDevToolPath))
 {
-    Write-Error "Can't find the Developer PowerShell launch script at '$LaunchDevToolPath'"
-    Return
+    Throw "Can't find the Developer PowerShell launch script at '$LaunchDevToolPath'"
 }
+
+$Cwd = Get-Location
 
 & $LaunchDevToolPath | Out-Null
-If (-not $?)
+
+Set-Location $Cwd
+
+$Tool = Get-Command -CommandType Application -ErrorAction SilentlyContinue $ToolName
+If (-not $Tool)
 {
-    Return
+    Throw "Can't find $ToolName"
 }
 
-$ToolPath = (Get-Command -CommandType Application -ErrorAction SilentlyContinue $ToolName).Source
-If (-not $ToolPath)
-{
-    Write-Error "Can't find $ToolName"
-    Return
-}
-
-$ToolPath
+$Tool.Source
